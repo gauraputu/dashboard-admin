9c5e3ffefb93a222f24dacecdc014247467814a5
gaura <gauraputu@gmail.com>
2024-11-17 00:06:09 +0800

    Update: removed cache files generated from django compressor

--- src/static/CACHE/js/output.64e44208ace6.js
+++
@@ -1,3 +0,0 @@
-(function(){const toggleSideBarButton=document.getElementById('toggleSidebarMobile');const parentElement=document.getElementById('sidebar');let showSidebar=false;function toggleSideBarDisplay(){const spans=parentElement.querySelectorAll('a span');if(showSidebar){spans.forEach(span=>{span.classList.add('hidden');});showSidebar=false;return}
-spans.forEach(span=>{span.classList.remove('hidden');});showSidebar=true;}
-toggleSideBarButton.addEventListener('click',()=>{toggleSideBarButton();})})();
--- src/static/CACHE/js/output.af8b1c573557.js
+++
@@ -1,3 +0,0 @@
-(function(){const toggleSideBarButton=document.getElementById('toggleSidebarMobile');const parentElement=document.getElementById('sidebar');let showSidebar=true;function toggleSideBarDisplay(){const spans=parentElement.querySelectorAll('a span');if(showSidebar){spans.forEach(span=>{span.classList.add('hidden');});parentElement.classList.replace('w-16','w-40');showSidebar=false;return}
-spans.forEach(span=>{span.classList.remove('hidden');});parentElement.classList.replace('w-40','w-16');showSidebar=true;}
-toggleSideBarButton.addEventListener('click',()=>{toggleSideBarDisplay();})})();
